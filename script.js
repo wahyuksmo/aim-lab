8f45d824ecceb15485117cd9612ee5d6ed41cac3
wahyu <wahyuackerman6@gmail.com>
2021-02-14 18:47:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const tanah = document.querySelectorAll('.tanah');
+const robot = document.querySelectorAll('.robot');
+const papanSkor = document.querySelector('#papan-skor');
+const pop = document.querySelector('#pop');
+let tanahSebelumnya;
+let selesai;
+let skor;
+
+function randomTanah(tanah) {
+    const t = Math.floor(Math.random() * tanah.length);
+    const tRandom = tanah[t];
+    if (tRandom == tanahSebelumnya) {
+        randomTanah(tanah);
+    }
+    tanahSebelumnya = tRandom;
+    return tRandom;
+}
+
+function randomWaktu(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function munculkanRobot() {
+    const tRandom = randomTanah(tanah);
+    const wRandom = randomWaktu(500, 500);
+    tRandom.classList.add('muncul');
+
+
+    setTimeout(() => {
+        tRandom.classList.remove('muncul');
+        if (!selesai) {
+            munculkanRobot();
+        }
+    }, wRandom);
+}
+
+function mulai() {
+    selesai = false;
+    skor = 0;
+    papanSkor.textContent = 0;
+    munculkanRobot();
+
+    setTimeout(() => {
+        selesai = true;
+    }, 80000);
+
+}
+
+function pukul() {
+    skor++;
+    pop.play();
+    this.parentNode.classList.remove('muncul');
+
+    papanSkor.textContent = skor;
+
+}
+
+robot.forEach(t => {
+    t.addEventListener('click', pukul);
+});
+
